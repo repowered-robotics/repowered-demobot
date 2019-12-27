2c0b5e3b5a8cde8505607905a913b27b38b50db0
AdamC19 <cordingleyadam19+github@gmail.com>
2019-12-27 00:09:38 +0000

    Add initial motor_ctrl code

---
+++ motor_ctrl/include/motor_ctrl.h
@@ -0,0 +1,15 @@
+#ifndef MOTOR_CTRL_H_
+#define MOTOR_CTRL_H_
+
+#include <ros/ros.h>
+#include <ros/spinner.h>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <inttypes.h>
+#include <iostream>
+#include <fstream>
+
+#include <wiringPi.h>
+
+#endif
---
+++ motor_ctrl/src/motor_ctrl_node.cpp
@@ -0,0 +1,9 @@
+#include <motor_ctrl.h>
+
+int main(int argc, char** argv){
+	ROS_INFO("Motor Control Node");
+	ros::init(argc, argv, "motor_ctrl");
+	ros::NodeHandle nh;
+
+	
+}
